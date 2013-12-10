f83235b373dde55e616ab13a279e28f11e1d509c
Mike Aldred <mike.aldred@luminousmonkey.org>
2013-12-10 08:54:41 +0800

    Unit tests pass. bin/unit_test now ignored in Git.

--- src/tests/brogue/map_test.c
+++ src/tests/brogue/map_test.c
@@ -14,12 +14,12 @@
 void testCoordinatesAreInMap() {
   CU_ASSERT_TRUE(coordinatesAreInMap(0, 0));
   CU_ASSERT_TRUE(coordinatesAreInMap(DCOLS / 2, DROWS / 2));
-  CU_ASSERT_TRUE(coordinatesAreInMap(DCOLS, DROWS));
+  CU_ASSERT_TRUE(coordinatesAreInMap(DCOLS - 1, DROWS - 1));
 
   CU_ASSERT_FALSE(coordinatesAreInMap(-1, 0));
   CU_ASSERT_FALSE(coordinatesAreInMap(0, -1));
   CU_ASSERT_FALSE(coordinatesAreInMap(-1, -1));
-  CU_ASSERT_FALSE(coordinatesAreInMap(DCOLS + 1, 0));
-  CU_ASSERT_FALSE(coordinatesAreInMap(0, DROWS + 1));
+  CU_ASSERT_FALSE(coordinatesAreInMap(DCOLS, 0));
+  CU_ASSERT_FALSE(coordinatesAreInMap(0, DROWS));
   CU_ASSERT_FALSE(coordinatesAreInMap(DCOLS, DROWS));
 }
